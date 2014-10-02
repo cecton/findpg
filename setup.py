af34a90e84e444dfa8cb6d2f234157e8cd02ff2d
Cécile Tonglet <cecile.tonglet@gmail.com>
2014-10-02 11:21:14 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name = "findpg",
+    version = "1.0",
+    author = "Cecile Tonglet",
+    author_email = "cecile.tonglet@gmail.com",
+    description = ("Find the best suited version of PostgreSQL to restore a "
+                   "dump"),
+    license = "MIT",
+    keywords = "postgres database dump restore",
+    url = "https://github.com/cecton/findpg",
+    py_modules=['findpg'],
+    scripts=['findpg.py'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Database",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
