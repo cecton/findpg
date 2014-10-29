3c7abaadab7c26d403f59febabee454206b06cc0
Cécile Tonglet <cecile.tonglet@gmail.com>
2014-10-29 14:02:33 +0100

    Release 2.0
    Change API to use an iterator instead of a file-object. Often more
    useful since a fileobject can be used as an iterator.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "findpg",
-    version = "1.0",
+    version = "2.0",
     author = "Cecile Tonglet",
     author_email = "cecile.tonglet@gmail.com",
     description = ("Find the best suited version of PostgreSQL to restore a "
