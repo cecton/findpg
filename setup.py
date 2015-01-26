51603dc1e063f148200d6d59d3f45ebff621ae23
Cécile Tonglet <cecile.tonglet@gmail.com>
2015-01-26 17:03:21 +0100

    Release 2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "findpg",
-    version = "2.1",
+    version = "2.2",
     author = "Cecile Tonglet",
     author_email = "cecile.tonglet@gmail.com",
     description = ("Find the best suited version of PostgreSQL to restore a "
